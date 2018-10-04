3d652a63c3c5c4953de74db1ba762a3a1ef6f52b
William Brown <william.brown-8@selu.edu>
2018-10-04 12:26:59 -0500

    Google sign in functions

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ export class Header extends Component {
 
           <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <div className="g-signin2" data-onsuccess="onSignIn"></div>
           </header>
 
           <div className="navbar">
@@ -25,6 +26,13 @@ export class Header extends Component {
   }
     }
 
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      }
 
 export default Header;
 
