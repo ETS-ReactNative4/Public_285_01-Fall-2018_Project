bd09a70860d85337f4ff610dfe46bd2af0c72431
William Brown <william.brown-8@selu.edu>
2018-10-19 22:44:06 -0500

    background video update

--- Src/src/components/Home.js
+++ Src/src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../stylesheets/Home.css';
 import Josh from "../Josh.jpg";
+import Waves from "../wavesBackground.mp4";
 
 export class Home extends Component {
     render() {
         return(
             <div className="Home">
+                <video class="backgroundVid" src ={Waves} autoplay="true" loop="true"></video>
 
                 <h1 className="title">Biography</h1>
 
