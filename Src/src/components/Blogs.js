5a7d0ba7e60c018154f3f618cf7894fabb9233d5
William Brown <william.brown-8@selu.edu>
2018-11-08 13:49:58 -0600

    Blog and Store update

--- Src/src/components/Blogs.js
+++ Src/src/components/Blogs.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import '../stylesheets/Home.css';
+import '../stylesheets/Blogs.css';
 
 export class Blogs extends Component {
     render() {
         return(
-            <div className="Home">
+            <div className="Blog">
 
-                <h1 className="title">Blogs</h1>
+                <h1 className="blogTitle">Problems in Politics</h1>
 
-                <p className="info">
-                        This is where Blog html goes
+                <p className="blogText">
+                        Things need to change right now. I am very experianced, 
+                        and I know what I am talking about. I am tired of the problems.
                 </p>
+                <div className="created">
+                created: October 30, 2018
+                </div>
+                <div className="comments">
+                    <div className="commentBody">
+                    testUser123: I disagree strongly.
+                    </div>
+                    <div className="commentDate">
+                    commented: October 31, 2018
+                    </div>
+                </div>
             </div>
         );
     }
--- Src/src/components/Store.js
+++ Src/src/components/Store.js
@@ -18,8 +18,9 @@ export class Store extends Component {
                          <h3>{books.products[0].title}</h3>
                           <p className="description">{books.products[0].description}</p>
                         <div>
-                            <div className="price">{books.products[0].price}$</div>
-                            <a href="#" className="addCart" role="button">Add to Cart</a>
+                            <div className="price">${books.products[0].price}.00</div>
+                            <a href="https://www.amazon.com/Great-Gatsby-F-Scott-Fitzgerald/dp/0743273567/ref=sr_1_1?s=books&ie=UTF8&qid=1541706084&sr=1-1&keywords=the+great+gatsby" 
+                            target="_blank" className="addCart" role="button">Buy</a>
                         </div>
                       </div>
                   </div>
@@ -32,8 +33,9 @@ export class Store extends Component {
                          <h3>{books.products[1].title}</h3>
                           <p className="description">{books.products[1].description}</p>
                         <div>
-                            <div className="price">{books.products[1].price}$</div>
-                            <a href="#" className="addCart" role="button">Add to Cart</a>
+                            <div className="price">${books.products[1].price}.00</div>
+                            <a href="https://www.amazon.com/Hatchet-Gary-Paulsen/dp/1416936475/ref=sr_1_3?s=books&ie=UTF8&qid=1541706149&sr=1-3&keywords=hatchet"
+                            target="_blank" className="addCart" role="button">Buy</a>
                         </div>
                       </div>
                   </div>
@@ -45,8 +47,9 @@ export class Store extends Component {
                          <h3>{books.products[2].title}</h3>
                           <p className="description">{books.products[2].description}</p>
                         <div>
-                            <div className="price">{books.products[2].price}$</div>
-                            <a href="#" className="addCart" role="button">Add to Cart</a>
+                            <div className="price">${books.products[2].price}.00</div>
+                            <a href="https://www.amazon.com/Casino-Royale-James-Bond-Fleming/dp/1612185436/ref=sr_1_1?s=books&ie=UTF8&qid=1541705941&sr=1-1&keywords=casino+royale"
+                            target="_blank" className="addCart" role="button">Buy</a>
                         </div>
                       </div>
                   </div>
